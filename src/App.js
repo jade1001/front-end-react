7d7920eb81015f7e09b4aab6ddff1b6764df8a07
Arron Deang <arron.deang@mcveanpacific.com>
2019-11-19 16:07:30 +0800

    added unfinished login form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.css'
+import Login from './components/login/Login'
 
 function App() {
-  return <div className='App'></div>
+    return (
+        <div className="App h-100">
+            <Login />
+        </div>
+    )
 }
 
 export default App
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,67 @@
 import React, { Component } from 'react'
+import { StyleSheet, css } from 'aphrodite'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIdCardAlt } from '@fortawesome/free-solid-svg-icons'
+import { faKey } from '@fortawesome/free-solid-svg-icons'
+
+const style = {
+    inputs: {
+        padding: '5px 15px',
+        fontFamily: 'Century Gothic',
+        border: '1px solid #ddd',
+        margin: 0,
+    },
+    icons: {
+        border: '1px solid #ddd',
+        borderRadius: 0,
+        backgroundColor: '#eee',
+    },
+}
 
 class Login extends Component {
-  render() {
-    return <div></div>
-  }
+    render() {
+        return (
+            <div className="row h-100 justify-content-center align-items-center">
+                <form className="mx-auto text-center">
+                    <h5>Treasury Debt Issuance</h5>
+                    <p>Login To Your Account</p>
+                    <div className="input-group form-group">
+                        <div className="input-group-prepend">
+                            <span
+                                className="input-group-text"
+                                style={style.icons}
+                            >
+                                <FontAwesomeIcon icon={faIdCardAlt} />
+                            </span>
+
+                            <input
+                                type="text"
+                                placeholder="User ID / Email"
+                                style={style.inputs}
+                            ></input>
+                        </div>
+                    </div>
+
+                    <div className="input-group form-group">
+                        <div className="input-group-prepend">
+                            <span
+                                className="input-group-text"
+                                style={style.icons}
+                            >
+                                <FontAwesomeIcon icon={faKey} />
+                            </span>
+
+                            <input
+                                type="text"
+                                placeholder="Password"
+                                style={style.inputs}
+                            ></input>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Login
