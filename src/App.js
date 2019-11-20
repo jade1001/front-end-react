2c14e8106430f09e1cf6097c3af5480545914871
Arron Deang <arron.deang@mcveanpacific.com>
2019-11-20 14:23:39 +0800

    testing app as homepage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React from 'react';
-import './App.css';
-import Login from './components/login/Login';
-import Nav from './components/nav/Nav';
-import Dashboard from './components/dashboard/Dashboard';
-import Home from './components/index/Home.js';
-import OpsNotif from './components/dashboard/OpsNotif';
+// import React from 'react'
+// import './App.css'
+// import Home from './components/index/Home.js'
 
-function App() {
-  return (
-    <div className='App h-100'>
-      {/* <Dashboard /> */}
-      {/* <Nav /> */}
-      {/* <Login /> */}
-      <Home />
-      {/* <OpsNotif /> */}
-    </div>
-  );
+// function App() {
+//     return (
+//         <div className="App h-100">
+//             <Home />
+//         </div>
+//     )
+// }
+
+// export default App
+
+import React, { Component } from 'react'
+import './App.css'
+import Home from './components/index/Home.js'
+import Dashboard from './components/dashboard/Dashboard'
+import Login from './components/login/Login'
+
+export class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: '',
+            password: '',
+            isLoggedIn: false,
+        }
+        this.signIn = this.signIn.bind(this)
+    }
+
+    signIn(email, password) {
+        this.setState({
+            email: email,
+            password: password,
+            isLoggedIn: true,
+        })
+    }
+    render() {
+        return (
+            <div className="App h-100">
+                {this.state.isLoggedIn ? (
+                    <Dashboard />
+                ) : (
+                    <Login signIn={this.signIn} />
+                )}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
