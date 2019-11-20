e759a6feea8a5d3d7dc7768dee734352898ab491
adrian1313 <55831474+adrian1313@users.noreply.github.com>
2019-11-20 16:35:18 +0800

    Updated UI and search

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
-import Login from './components/login/Login';
-import Nav from './components/nav/Nav';
-import Dashboard from './components/dashboard/Dashboard';
-import Home from './components/index/Home.js';
-import OpsNotif from './components/dashboard/OpsNotif';
+// import Login from './components/login/Login';
+// import Nav from './components/nav/Nav';
+// import Dashboard from './components/dashboard/Dashboard';
+// import Home from './components/index/Home.js';
+// import OpsNotif from './components/dashboard/OpsNotif';
 import DebtIssuance from './components/DebtIssuanceTab/DebtIssuance';
 
 function App() {
   return (
     <div className='App h-100'>
       {/* <Dashboard /> */}
-      {/* <Nav /> */}
+      {/* <Nav />  */}
       {/* <Login /> */}
-      <Home />
+      <DebtIssuance />
       {/* <DebtIssuance /> */}
       {/* <Tables /> */}
       {/* <OpsNotif /> */}
